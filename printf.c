6fb1fc0076afe981a3b415a86f2d11490265abab
Lincon <lincon1000ls@gmail.com>
2024-03-09 22:53:41 -0300

    Usage of printf

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("hi, how u doin?\n");
+
+    printf("Valor inteiro: %d. \n", 10);
+    printf("Valor real: %f.\n", 3.14159265);
+    printf("Valor real com apenas duas casas: %.2f\n",3.14149265);
+    printf("Dado de texto %c.\n",'a');
+    printf("Dado de texto %s.\n", "testando");
+}
